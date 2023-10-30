ddcbbf3e907245a12dd8b7514cafd954094c1c9f
Tyler Bisk <tyler07039@gmail.com>
2023-10-30 19:50:29 -0400

    google lens from github

--- processing/lens/main.js
+++ processing/lens/main.js
@@ -2,43 +2,43 @@ import puppeteer from "puppeteer-extra";
 import StealthPlugin from "puppeteer-extra-plugin-stealth";
 
 puppeteer.use(StealthPlugin());
+
 const searchParams = {
-  imageUrl: "https://github.com/tylerbisk/cv-birdfeeder/blob/master/img/1.png", //Parameter defines the URL of an image to perform the Google Lens search
+  imageUrl: "http://raw.githubusercontent.com/tylerbisk/cv-birdfeeder/master/img/3.png", //Parameter defines the URL of an image to perform the Google Lens search
   hl: "en", //Parameter defines the language to use for the Google search
   gl: "US",
 };
+
 const URL = `https://lens.google.com/uploadbyurl?url=${searchParams.imageUrl}&hl=${searchParams.hl}&gl=${searchParams.gl}`;
+
+
 async function getResultsFromPage(page) {
-  const image = await page.$$(".bn6k9b");
-  await Promise.all(image.map(async (t) => { await t.click(); }));
-  const knowledgeGraphItems = await page.$$(".DeMn2d");
-  const knowledgeGraph = [];
-  for (const item of knowledgeGraphItems) {
-    const label = await item.evaluate(x => x.textContent);
-    console.log(label)
-    const handles = await Promise.all(knowledgeGraphItems.map(handle => handle.getProperty("src")));
-    const label2 = handle.textContent;
-    console.log(label2)
-    await page.waitForTimeout(2000);
-    const thumbnail = await item.$eval(".FH8DCc", (node) => node.getAttribute("src"));
-    knowledgeGraph.push(
-      await page.evaluate(
-        (thumbnail) => ({
-          title: document.querySelector(".DeMn2d").textContent,
-          subtitle: document.querySelector(".XNTym").textContent,
-        }),
-        thumbnail
-      )
-    );
-  }
-  return { knowledgeGraph };
+  const labels = await page.$$(".DeMn2d");
+  
+  let label = await Promise.all(labels.map(async (t) => {
+      return await t.evaluate(x => x.textContent);
+  }))
+
+  let sublabels = await page.$$(".XNTym");
+
+  let sublabel = await Promise.all(sublabels.map(async (t) => {
+    return await t.evaluate(x => x.textContent);
+  }))
+
+  //await page.waitForTimeout(2000);
+
+  //console.log(label);
+  //console.log(sublabel);
+  
+  return label;
 }
+
+
 async function getLensResults() {
   const browser = await puppeteer.launch({
-    headless: true, // if you want to see what the browser is doing, you need to change this option to "false"
     args: ["--no-sandbox", "--disable-setuid-sandbox"],
     executablePath: '/usr/bin/chromium-browser',
-    headless: "new",
+    headless: "new", // false for debuggung to see what is going on
   });
   const page = await browser.newPage();
   await page.setDefaultNavigationTimeout(60000);
@@ -47,4 +47,6 @@ async function getLensResults() {
   await browser.close();
   return results;
 }
+
+
 getLensResults().then((result) => console.dir(result, { depth: null }));
